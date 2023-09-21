d0fce1c67eeeae84b818fdace5084e620ed20dea
ronalcastro97 <105658996+ronalcastro97@users.noreply.github.com>
2023-09-21 13:06:23 -0500

    Update Mamifero.java

--- src/test/java/zooAnimales/Mamifero.java
+++ src/test/java/zooAnimales/Mamifero.java
@@ -57,7 +57,7 @@ public class Mamifero extends Animal{
 	public void setPatas(int patas) {
 		this.patas = patas; 
 	} 
-	public Mamifero crearCaballo(String nombre,int edad,String genero) {
+	public static Mamifero crearCaballo(String nombre,int edad,String genero) {
 		
 		Mamifero caballo= new Mamifero(nombre,edad,"Pradera",genero,true,4); 
 		Mamifero.caballos++;
@@ -65,12 +65,12 @@ public class Mamifero extends Animal{
 		 
 		
 	} 
-	public Mamifero crearLeon(String nombre,int edad,String genero) {
+	public static Mamifero crearLeon(String nombre,int edad,String genero) {
 		Mamifero leon= new Mamifero(nombre,edad,"selva",genero,true,4);
 		Mamifero.leones++;
 		return leon;
 	}
-	public int cantidadMamiferos() {
+	public static int cantidadMamiferos() {
 		return Mamifero.getListado().size();
 	}
 	}
