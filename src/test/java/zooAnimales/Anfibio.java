8caa929482a5f5e69a5a2c95bb03968a972ed2bc
ronalcastro97 <105658996+ronalcastro97@users.noreply.github.com>
2023-09-21 15:55:37 -0500

    taller java-5

--- src/test/java/zooAnimales/Anfibio.java
+++ src/test/java/zooAnimales/Anfibio.java
@@ -1,68 +1,68 @@
-package zooAnimales;
-import gestion.Zona;
-import java.util.ArrayList;
-
-public class Anfibio extends Animal{
-	private static ArrayList<Anfibio>	listado;
-	public static int ranas=0;
-	public static int salamandras=0;
-	private String colorPiel;
-	private boolean venenoso;
-
-	public Anfibio() {}
+	package zooAnimales;
+	import gestion.Zona;
+	import java.util.ArrayList;
 	
-	public Anfibio(String nombre,int edad,String habitat,String genero,String colorPiel,boolean venenoso) {
-		super(nombre,edad,habitat,genero);
-		this.colorPiel = colorPiel;
-		this.venenoso = venenoso;
-		this.listado=new ArrayList<>();
+	public class Anfibio extends Animal{
+		private static ArrayList<Anfibio>	listado;
+		public static int ranas=0;
+		public static int salamandras=0;
+		private String colorPiel;
+		private boolean venenoso;
 	
+		public Anfibio() {}
+		
+		public Anfibio(String nombre,int edad,String habitat,String genero,String colorPiel,boolean venenoso) {
+			super(nombre,edad,habitat,genero);
+			this.colorPiel = colorPiel;
+			this.venenoso = venenoso;
+			this.listado=new ArrayList<>();
+		
+		}
+	
+		public  static ArrayList<Anfibio> getListado() {
+			return listado;
+		}
+		public static void setListado(ArrayList<Anfibio> listado) {
+			Anfibio.listado = listado;
+		}
+		public static int getRanas() {
+			return ranas;
+		}
+		public static void setRanas(int ranas) {
+			Anfibio.ranas = ranas;
+		}
+		public static int getSalamandras() {
+			return salamandras;
+		}
+		public static void setSalamandras(int salamandras) {
+			Anfibio.salamandras = salamandras;
+		}
+		public String getColorPiel() {
+			return colorPiel;
+		}
+		public void setColorPiel(String colorPiel) {
+			this.colorPiel = colorPiel;
+		}
+		public boolean isVenenoso() {
+			return venenoso;
+		}
+		public void setVenenoso(boolean venenoso) {
+			this.venenoso = venenoso;
+		}
+		public String movimiento() {
+			return "saltar";	
 	}
-
-	public  static ArrayList<Anfibio> getListado() {
-		return listado;
+		public Anfibio crearRana(String nombre,int edad,String genero) {
+			Anfibio rana=new Anfibio(nombre,edad,"selva",genero,"rojo",true);
+			Anfibio.ranas++;
+			return rana;
+		}
+		public Anfibio crearSalamandra(String nombre,int edad,String genero) {
+			Anfibio salamandra=new Anfibio(nombre,edad,"selva",genero,"negro y amarillo",false);
+			Anfibio.salamandras++;
+			return salamandra;
+		}	
+		public int cantidadAnfibios() {
+			return Anfibio.getListado().size();
+		}
 	}
-	public static void setListado(ArrayList<Anfibio> listado) {
-		Anfibio.listado = listado;
-	}
-	public static int getRanas() {
-		return ranas;
-	}
-	public static void setRanas(int ranas) {
-		Anfibio.ranas = ranas;
-	}
-	public static int getSalamandras() {
-		return salamandras;
-	}
-	public static void setSalamandras(int salamandras) {
-		Anfibio.salamandras = salamandras;
-	}
-	public String getColorPiel() {
-		return colorPiel;
-	}
-	public void setColorPiel(String colorPiel) {
-		this.colorPiel = colorPiel;
-	}
-	public boolean isVenenoso() {
-		return venenoso;
-	}
-	public void setVenenoso(boolean venenoso) {
-		this.venenoso = venenoso;
-	}
-	public String movimiento() {
-		return "saltar";	
-}
-	public static Anfibio crearRana(String nombre,int edad,String genero) {
-		Anfibio rana=new Anfibio(nombre,edad,"selva",genero,"rojo",true);
-		Anfibio.ranas++;
-		return rana;
-	}
-	public static Anfibio crearSalamandra(String nombre,int edad,String genero) {
-		Anfibio salamandra=new Anfibio(nombre,edad,"selva",genero,"negro y amarillo",false);
-		Anfibio.salamandras++;
-		return salamandra;
-	}	
-	public static int CantidadAnfibios() {
-		return Anfibio.listado.size();
-	}
-}
--- src/test/java/zooAnimales/Pez.java
+++ src/test/java/zooAnimales/Pez.java
@@ -1,68 +1,68 @@
-package zooAnimales;
-
-import java.util.ArrayList;
-
-public class Pez extends Animal{
-	private static ArrayList<Pez>	listado;
-	public static int salmones=0;
-	public static int  bacalaos=0;
-	private String colorEscamas;
-	private int cantidadAletas;
-	public Pez() {
-		super();
-
-	}
-	public Pez(String nombre, int edad, String habitat, String genero,String colorEscamas, int cantidadAletas) {
-		super(nombre, edad, habitat, genero);
-		this.colorEscamas=colorEscamas;
-		this.cantidadAletas=cantidadAletas;
-		this.listado=new ArrayList<>();
-	}
-	public static ArrayList<Pez> getListado() {
-		return listado;
-	}
-	public static void setListado(ArrayList<Pez> listado) {
-		Pez.listado = listado;
-	}
-	public static int getSalmones() {
-		return salmones;
-	}
-	public static void setSalmones(int salmones) {
-		Pez.salmones = salmones;
-	}
-	public static int getBacalaos() {
-		return bacalaos;
-	}
-	public static void setBacalaos(int bacalaos) {
-		Pez.bacalaos = bacalaos;
-	}
-	public String getColorEscamas() {
-		return colorEscamas;
-	}
-	public void setColorEscamas(String colorEscamas) {
-		this.colorEscamas = colorEscamas;
-	}
-	public int getCantidadAletas() {
-		return cantidadAletas;
-	}
-	public void setCantidadAletas(int cantidadAletas) {
-		this.cantidadAletas = cantidadAletas;
-	}
+	package zooAnimales;
 	
-	public String movimiento() {
-		return "nadar";	
-}
-	public static Pez crearSalmon(String nombre,int edad,String genero) {
-		Pez salmon=new Pez(nombre,edad,"oceano",genero,"rojo",6);
-		Pez.salmones++;
-		return salmon;
+	import java.util.ArrayList;
+	
+	public class Pez extends Animal{
+		private static ArrayList<Pez>	listado;
+		public static int salmones=0;
+		public static int  bacalaos=0;
+		private String colorEscamas;
+		private int cantidadAletas;
+		public Pez() {
+			super();
+	
+		}
+		public Pez(String nombre, int edad, String habitat, String genero,String colorEscamas, int cantidadAletas) {
+			super(nombre, edad, habitat, genero);
+			this.colorEscamas=colorEscamas;
+			this.cantidadAletas=cantidadAletas;
+			this.listado=new ArrayList<>();
+		}
+		public static ArrayList<Pez> getListado() {
+			return listado;
+		}
+		public static void setListado(ArrayList<Pez> listado) {
+			Pez.listado = listado;
+		}
+		public static int getSalmones() {
+			return salmones;
+		}
+		public static void setSalmones(int salmones) {
+			Pez.salmones = salmones;
+		}
+		public static int getBacalaos() {
+			return bacalaos;
+		}
+		public static void setBacalaos(int bacalaos) {
+			Pez.bacalaos = bacalaos;
+		}
+		public String getColorEscamas() {
+			return colorEscamas;
+		}
+		public void setColorEscamas(String colorEscamas) {
+			this.colorEscamas = colorEscamas;
+		}
+		public int getCantidadAletas() {
+			return cantidadAletas;
+		}
+		public void setCantidadAletas(int cantidadAletas) {
+			this.cantidadAletas = cantidadAletas;
+		}
+		
+		public String movimiento() {
+			return "nadar";	
 	}
-	public static Pez crearBacalao(String nombre,int edad,String genero) {
-		Pez bacalao=new Pez(nombre,edad,"oceano",genero,"gris",6);
-		Pez.bacalaos++;
-		return bacalao;
+		public static Pez crearSalmon(String nombre,int edad,String genero) {
+			Pez salmon=new Pez(nombre,edad,"oceano",genero,"rojo",6);
+			Pez.salmones++;
+			return salmon;
+		}
+		public static Pez crearBacalao(String nombre,int edad,String genero) { 
+			Pez bacalao=new Pez(nombre,edad,"oceano",genero,"gris",6); 
+			Pez.bacalaos++;
+			return bacalao;
+		}
+		public static int CantidadPeces() {
+			return Pez.listado.size();
+		}
 	}
-	public static int CantidadPeces() {
-		return Pez.listado.size();
-	}
-}
